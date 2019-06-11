1157707d13c907a26f2dc6312fc83d2e9cfc64b8
Heniker <art_saif@mail.ru>
2019-06-11 05:50:01 +0300

    initial

--- README.md
+++
@@ -1,6 +0,0 @@
-# Rubiks-cube
-
-https://greedyender.github.io/Rubiks-cube/
----
-3D Rubiks cube in browser with [**ThreeJS**](https://github.com/mrdoob/three.js/).  
-*Adapted for mobile devices.*
--- src/ts/events/dispatchers/cubeDragStartDispatcher.ts
+++
@@ -1,9 +0,0 @@
-import {intersects} from '../../globals'
-import DragOnCubeEvent from '../DragOnCubeEvent'
-
-
-window.addEventListener('pointerdown', () => {
-	if (intersects.length) {
-		window.dispatchEvent(new DragOnCubeEvent('cubedragstart', intersects[0]))
-	}
-})
